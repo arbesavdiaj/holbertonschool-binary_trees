d82663d7dfbc66055155222f7823691821434739
Ferlandoo <contact@ferlando.me>
2024-01-08 14:32:35 +0100

    update betty and ex 10

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -11,27 +11,25 @@
 
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 {
-binary_tree_t *new_node = NULL;
+	binary_tree_t *new_node = NULL;
 
-if (!parent)
-return (NULL);
+	if (!parent)
+		return (NULL);
 
-new_node = malloc(sizeof(binary_tree_t));
+	new_node = malloc(sizeof(binary_tree_t));
 
-if (!new_node)
-return (NULL);
+	if (!new_node)
+		return (NULL);
 
-new_node->n = value;
-new_node->parent = parent;
-new_node->right = NULL;
+	new_node->n = value;
+	new_node->parent = parent;
+	new_node->right = NULL;
 
-if (parent->left)
-{
-new_node->left = parent->left;
-parent->left->parent = new_node;
-}
-
-parent->left = new_node;
-
-return (new_node);
+	if (parent->left)
+	{
+	new_node->left = parent->left;
+	parent->left->parent = new_node;
+	}
+	parent->left = new_node;
+	return (new_node);
 }
