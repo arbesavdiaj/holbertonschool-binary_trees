331c1cb486f9a12ec53e7c703968c62203bcf85e
Ferlandoo <contact@ferlando.me>
2024-01-08 15:05:28 +0100

    add size t

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -8,13 +8,35 @@
 
 int binary_tree_balance(const binary_tree_t *tree)
 {
-	int left = 0, right = 0;
+	int left_height = 0, right_height = 0;
 
 	if (tree == NULL)
 		return (0);
 
-	left = binary_tree_height(tree->left);
-	right = binary_tree_height(tree->right);
+	left_height = binary_tree_height(tree->left);
+	right_height = binary_tree_height(tree->right);
 
-	return (left - right);
+	return (left_height - right_height);
+}
+
+/**
+ * binary_tree_height - measures the height of a binary tree
+ * @tree: pointer to the root node of the tree
+ * Return: height of the tree
+ */
+
+size_t binary_tree_height(const binary_tree_t *tree)
+{
+	size_t left_height = 0, right_height = 0;
+
+	if (tree == NULL)
+		return (0);
+
+	left_height = binary_tree_height(tree->left);
+	right_height = binary_tree_height(tree->right);
+
+	if (left_height > right_height)
+		return (left_height + 1);
+
+	return (right_height + 1);
 }
