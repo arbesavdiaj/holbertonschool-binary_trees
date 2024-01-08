8e3dcb37209a4e14373ee19f41371d029cc72589
arbesavdiaj <arbesavdiaj14@gmail.com>
2024-01-08 11:48:33 +0100

    fix code

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -11,24 +11,26 @@
 
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 {
-    binary_tree_t *new_node = NULL, *tmp = NULL;
+if (parent == NULL)
+return (NULL);
 
-    if (!parent)
-        return (NULL);
+binary_tree_t *new_node = malloc(sizeof(binary_tree_t));
 
-    new_node = malloc(sizeof(binary_tree_t));
-    if (!new_node)
-        return (NULL);
+if (new_node == NULL)
+return (NULL);
 
-    tmp = parent->left;
-    parent->left = new_node;
-    new_node->parent = parent;
-    new_node->n = value;
-    new_node->left = tmp;
-    new_node->right = NULL;
+new_node->n = value;
+new_node->parent = parent;
+new_node->right = NULL;
 
-    if (tmp)
-        tmp->parent = new_node;
+if (parent->left != NULL)
+{
+new_node->left = parent->left
+parent->left->parent = new_node;
+}
+new_node->left = NULL;
 
-    return (new_node);
+parent->left = new_node;
+
+return (new_node);
 }
