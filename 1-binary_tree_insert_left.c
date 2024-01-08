91886379618d28cfac20b84fc73a04f54a283229
Ferlandoo <contact@ferlando.me>
2024-01-09 00:07:34 +0100

    edit code

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -16,20 +16,18 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 	if (!parent)
 		return (NULL);
 
-	new_node = malloc(sizeof(binary_tree_t));
+	new_node = binary_tree_node(parent, value);
 
 	if (!new_node)
 		return (NULL);
 
-	new_node->n = value;
-	new_node->parent = parent;
-	new_node->right = NULL;
-
 	if (parent->left)
 	{
-	new_node->left = parent->left;
-	parent->left->parent = new_node;
+		new_node->left = parent->left;
+		parent->left->parent = new_node;
 	}
+
 	parent->left = new_node;
+
 	return (new_node);
 }
